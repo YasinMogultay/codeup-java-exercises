32a6d59da28df42f0278bb172d8383743f074c80
Yasin Mogultay <yasin.mglty@gmail.com>
2021-03-05 12:26:55 -0600

    Completed StringExercise

---
+++ src/StringExercise.java
@@ -0,0 +1,20 @@
+public class StringExercise {
+   public static void main(String[] args){
+       //Question 1
+       String word1 = "We don't need no education \nWe don't need no thought control";
+       System.out.println(word1);
+
+       //Question 2
+       String word2 = "Check \"this\" out!, \"s inside of \"s!"; //if you use backward slash before double quote you can get expected output
+       System.out.println(word2);
+
+       //Question 3
+       String word3 = "In windows, the main drive is usually C:\\"; // if you wanna use backward slash in string you can achive that double backward slash
+       System.out.println(word3);
+
+       //Question 4
+       String word4 = "I can do backslashes \\, double backslashes \\\\, \nand the amazing triple backslash\\\\\\!";
+       System.out.println(word4);
+
+   }
+}
