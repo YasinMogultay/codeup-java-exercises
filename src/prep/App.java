f966a71ffc19003c984c1f0aba74fb297622102e
Yasin Mogultay <yasin.mglty@gmail.com>
2021-03-22 20:44:32 -0500

    pre assesment done

--- src/prep/App.java
+++ src/prep/App.java
@@ -1,26 +1,28 @@
 package prep;
 
+
+import prep.Foods.FoodTools;
+
 public class App {
     public static void main(String[] args) {
 
         //Question 1
-        System.out.println("power of number: " + Arithmetic.square(2));
+        System.out.println("power of number: " + Arithmetic.square(5));
 
         //Question 2
-        System.out.println("sum of numbers: " + Arithmetic.sum(3,3));
+        System.out.println("sum of numbers: " + Arithmetic.sum(3, 3));
 
         //Question 3
-        System.out.println("difference of numbers: " +Arithmetic.difference(10,2));
+        System.out.println("difference of numbers: " + Arithmetic.difference(10, 2));
 
         //Question 3
-        System.out.println("product of numbers: " + Arithmetic.product(2,5));
+        System.out.println("product of numbers: " + Arithmetic.product(2, 5));
 
         //Question 4
         double[] numbers = {1, 2, 3, 4, 5};
-            System.out.println("average of arrays : " + Arithmetic.average(numbers));
+        System.out.println("average array of numbers : " + Arithmetic.average(new double[]{1, 2, 3})); // or callnumbers
 
-            Food food = new Food("asd","",1);
-
+        FoodTools.initializeApp();
 
     }
 }
---
+++ src/prep/Foods/Consumable.java
@@ -0,0 +1,9 @@
+package prep.Foods;
+
+
+import java.util.List;
+
+public interface Consumable {
+    void  consume();
+    List<String> getFullDescription();
+}
